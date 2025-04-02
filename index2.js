fc42dbd414c19c4c3e2b6186267d4c2d2b3bd62a
Aman Kumar <129107515+amnraz@users.noreply.github.com>
2025-04-03 01:46:23 +0530

    global Execution context

--- index2.js
+++ index2.js
@@ -1,3 +1,8 @@
+
+const profile = { name: "Charlie", age: 29, address: { city: "San Francisco", zipcode: "94101" } };
+
+const updates = { age: 30, address: { zipcode: "94109", country: "USA" } };
+
 let result = {
         name : profile.name,
         age: updates.age,
