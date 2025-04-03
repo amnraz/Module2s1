465bd3a3727d4442381d34af5c043f2db2b49b75
Aman Kumar <129107515+amnraz@users.noreply.github.com>
2025-04-03 21:46:23 +0530

    Fixing array and object operations

--- ass2q11.js
+++ ass2q11.js
@@ -1,11 +1,11 @@
-var  age = 25;
-function displayAge(){
-    console.log(age)
-}
+// var  age = 25;
+// function displayAge(){
+//     console.log(age)
+// }
 
-function changeAge(){
-    age = 35
-}
-displayAge()
-changeAge()
-console.log(age)
+// function changeAge(){
+//     age = 35
+// }
+// displayAge()
+// changeAge()
+// console.log(age)
